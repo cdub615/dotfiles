16fe549e0d5732ba8c47547b17decd3822d5a43b
Christian White <christianbwhite@gmail.com>
2024-10-13 21:16:20 -0500

    Update .zshrc Update qmk_firmware/keyboards/3w6/rev1/config.h Update qmk_firmware/keyboards/3w6/rev1/matrix.c Update qmk_firmware/keyboards/3w6/rev1/rules.mk Update qmk_firmware/keyboards/3w6/rev2/keymaps/default_pimoroni/pimoroni_trackball.c Update qmk_firmware/keyboards/3w6/rev2/keymaps/default_pimoroni/pimoroni_trackball.h Update qmk_firmware/keyboards/3w6/rev2/matrix.c Update qmk_firmware/keyboards/3w6/rev2/rules.mk

--- qmk_firmware/keyboards/3w6/rev1/config.h
+++ qmk_firmware/keyboards/3w6/rev1/config.h
@@ -43,8 +43,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* COL2ROW, ROW2COL */
 #define DIODE_DIRECTION COL2ROW
-
-
-#define TAPPING_TERM_PER_KEY
-#define OS_DETECTION_DEBOUNCE 2000
-#define OS_DETECTION_KEYBOARD_RESET
